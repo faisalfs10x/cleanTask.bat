b6d10d3b28e68cc04cf421d1227f841d205381d1
Faisalfs10x <51811615+faisalfs10x@users.noreply.github.com>
2020-10-08 10:10:06 +0800

    Update rm.bat

--- rm.bat
+++ rm.bat
@@ -1,4 +1,4 @@
 TIMEOUT /T 10
-start cmd.exe /c del /f webapps\birt\x.jsp
+start cmd.exe /c del /f C:\Users\delfx\Desktop\webapps\birt\webapps\birt\x.jsp
 start cmd.exe /c del /f webapps\birt\rm.bat
 exit
